b7e631448ba25ff56796ee2b8f42fd017fd16792
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-07 21:54:59 +0000

    adding a bit of code and an associated demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/ch05-fmc.r
@@ -0,0 +1,15 @@
+# function for sampling a finite Markov chain
+
+rfmc<-function(n,P,pi0)
+{
+        v=vector("numeric",n)
+        r=length(pi0)
+        v[1]=sample(r,1,prob=pi0)
+        for (i in 2:n) {
+                v[i]=sample(r,1,prob=P[v[i-1],])
+        }
+        ts(v)
+}
+
+# eof
+
