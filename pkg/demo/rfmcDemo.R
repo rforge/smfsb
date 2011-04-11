41e5bb9fc251251f9d86d0761ac51ba606f3885a
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-11 20:19:28 +0000

    tweak to demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/rfmcDemo.R
+++ pkg/demo/rfmcDemo.R
@@ -1,8 +1,16 @@
 # demo for sampling a finite Markov chain
 
+# load the package
+library(smfsb)
+
+# set up a Markov transition structure
 P=matrix(c(0.9,0.1,0.2,0.8),ncol=2,byrow=TRUE)
 pi0=c(0.5,0.5)
+
+# generate a sample
 samplepath=rfmc(200,P,pi0)
+
+# analyse the sample
 plot(samplepath)
 summary(samplepath)
 table(samplepath)
