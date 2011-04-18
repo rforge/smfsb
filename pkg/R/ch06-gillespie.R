5f765477374f2d86a706f2462275826ff5236357
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-18 20:00:21 +0000

    added functions for basic gillespie simulation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/ch06-gillespie.R
@@ -0,0 +1,46 @@
+
+# function
+
+gillespie=function (N, n, ...)
+{
+        tt=0
+        x=N$M
+        S=t(N$Post-N$Pre)
+        u=nrow(S)
+        v=ncol(S)
+        tvec=vector("numeric",n)
+        xmat=matrix(0,ncol=u,nrow=n+1)
+        xmat[1,]=x
+        for (i in 1:n) {
+                h=N$h(tt,x, ...)
+                tt=tt+rexp(1,sum(h))
+                j=sample(v,1,prob=h)
+                x=x+S[,j]
+                tvec[i]=tt
+                xmat[i+1,]=x
+        }
+        return(list(t=tvec,x=xmat))
+}
+
+discretise=function(out,dt=1,start=0)
+{
+        events=length(out$t)
+        end=out$t[events]
+        len=(end-start)%/%dt+1
+        x=matrix(0,nrow=len,ncol=ncol(out$x))
+        target=0
+        j=1
+        for (i in 1:events) {
+                while (out$t[i]>=target) {
+                        x[j,]=out$x[i,]
+                        j=j+1
+                        target=target+dt
+                }
+        }
+        ts(x,start=0,deltat=dt)
+}
+
+# example
+
+# eof
+
