20130490bba3e0fed1dde7318a89962a219229f4
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-18 21:12:49 +0000

    Updated vignette and project web page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> See the web page for the book <a href="http://www.staff.ncl.ac.uk/d.j.wilkinson/smfsb/2e/">Stochastic Modelling for Systems Biology, second edition</a> for further details about this R package. </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
