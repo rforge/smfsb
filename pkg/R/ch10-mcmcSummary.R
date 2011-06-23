d80c9af31e4543f5232b1b0b14cf1c79b37a135d
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-23 15:05:11 +0000

    code for mcmc summaries - no documentation for it yet...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ch10-mcmcSummary.R
+++ pkg/R/ch10-mcmcSummary.R
@@ -11,9 +11,9 @@ mcmcSummary <- function(mat,rows=4,plot=TRUE)
     op=par(mfrow=c(rows,3))
     names=colnames(mat)
     for (i in 1:p) {
-      plot(ts(mat[,i]),main=names[i])
+      plot(ts(mat[,i]),main=names[i],ylab="Value",xlab="Iteration")
       acf(mat[,i],lag.max=100,main=names[i])
-      hist(mat[,i],30,main=names[i])
+      hist(mat[,i],30,main=names[i],xlab="Value",freq=FALSE)
     }
     par(op)
   }
