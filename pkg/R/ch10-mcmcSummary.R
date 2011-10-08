af66d8b6529a94b4342b30b9882ffd317f117eab
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-08 17:14:52 +0000

    finished mcmcSummary and added documentation for data sets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ch10-mcmcSummary.R
+++ pkg/R/ch10-mcmcSummary.R
@@ -1,22 +1,27 @@
 # mcmcSummary
 
-mcmcSummary <- function(mat,rows=4,plot=TRUE)
-  {
-    d=dim(mat)
-    p=d[2]
-    message(paste("N =",d[1],"iterations"))
-    print(summary(mat))
-    message("Standard deviations:")
-    print(apply(mat,2,sd))
-    op=par(mfrow=c(rows,3))
-    names=colnames(mat)
-    for (i in 1:p) {
-      plot(ts(mat[,i]),main=names[i],ylab="Value",xlab="Iteration")
-      acf(mat[,i],lag.max=100,main=names[i])
-      hist(mat[,i],30,main=names[i],xlab="Value",freq=FALSE)
+mcmcSummary<-function (mat, rows = 4, lag.max=100, bins=30, show = TRUE, plot = TRUE) 
+{
+    d = dim(mat)
+    p = d[2]
+    summ=summary(mat)
+    if (show==TRUE) {
+        message(paste("N =", d[1], "iterations"))
+	print(summ)
+	message("Standard deviations:")
+        print(apply(mat, 2, sd))
     }
-    par(op)
-  }
-
+    if (plot==TRUE) {
+        names = colnames(mat)
+	op = par(mfrow = c(rows, 3))
+	for (i in 1:p) {
+        	plot(ts(mat[, i]), main = names[i], ylab = "Value", xlab = "Iteration")
+       	 	acf(mat[, i], lag.max = lag.max, main = names[i])
+        	hist(mat[, i], bins, main = names[i], xlab = "Value", freq = FALSE)
+	}
+    	par(op)
+    }
+    invisible(summ)
+}
 
 # eof
