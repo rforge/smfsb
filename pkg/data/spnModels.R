2665a3bb2f7526d6d4840e225452c12049f498f2
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-24 20:35:54 +0000

    added a Michaelis-Menten enzyme kinetic model to spnModels
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/data/spnModels.R
+++ pkg/data/spnModels.R
@@ -7,9 +7,9 @@ LV$Post=matrix(c(2,0,0,2,0,0),ncol=2,byrow=TRUE)
 LV$M=c(x1=50,x2=100)
 LV$h=function(x,t,th=c(th1=1,th2=0.005,th3=0.6))
 {
- with(as.list(c(x,th)),{
-         return(c(th1*x1, th2*x1*x2, th3*x2 ))
-        })
+  with(as.list(c(x,th)),{
+    return(c(th1*x1, th2*x1*x2, th3*x2 ))
+  })
 }
 
 # Imigration-Death process
@@ -18,9 +18,9 @@ ID$Pre=matrix(c(0,1),ncol=1)
 ID$Post=matrix(c(1,0),ncol=1)
 ID$h=function(x,t,th=c(lambda=1,mu=0.1))
 {
- with(as.list(c(x,th)),{
-  return(c(lambda,mu*x))
- })
+  with(as.list(c(x,th)),{
+    return(c(lambda,mu*x))
+  })
 }
 
 # Birth-Death process
@@ -29,20 +29,32 @@ BD$Pre=matrix(c(1,1),ncol=1)
 BD$Post=matrix(c(2,0),ncol=1)
 BD$h=function(x,t,th=c(lambda=1,mu=1.1))
 {
- with(as.list(c(x,th)),{
-  return(c(lambda*x,mu*x))
- })
+  with(as.list(c(x,th)),{
+    return(c(lambda*x,mu*x))
+  })
 }
 
+# Dimerisation kinetics
 Dimer=list()
 Dimer$Pre=matrix(c(2,0,0,1),ncol=2,byrow=TRUE)
 Dimer$Post=matrix(c(0,1,2,0),ncol=2,byrow=TRUE)
+Dimer$M=c(x1=301,x2=0)
 Dimer$h=function(x,t,th=c(th1=1.66e-3,th2=0.2)) {
- with(as.list(c(x,th)),{
-        return(c(th1*x1*(x1-1)/2, th2*x2))
-	})	
-    }
+  with(as.list(c(x,th)),{
+    return(c(th1*x1*(x1-1)/2, th2*x2))
+  })	
+}
 
+# Michaelis-Menten kinetics
+MM=list()
+MM$Pre=matrix(c(1,1,0,0,0,0,1,0,0,0,1,0),ncol=4,byrow=TRUE)
+MM$Post=matrix(c(0,0,1,0,1,1,0,0,0,1,0,1),ncol=4,byrow=TRUE)
+MM$M=c(S=301,E=120,SE=0,P=0)
+MM$h=function(x,t,th=c(c1=1.66e-3,c2=1e-4,c3=0.1)) {
+  with(as.list(c(x,th)),{
+    return(c(c1*S*E, c2*SE, c3*SE))
+  })
+}
 
 
 
