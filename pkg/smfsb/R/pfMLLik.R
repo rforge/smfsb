8c3c4dcbdfa0f80dd0b89e4c84a24a386460606a
darrenjw <darrenjw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-05-28 14:47:44 +0000

    Log-sum-exp version of the particle filter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/smfsb@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/smfsb/R/pfMLLik.R
+++ pkg/smfsb/R/pfMLLik.R
@@ -1,5 +1,5 @@
-# pfMLLik
-# particle filter unbiased estimate of marginal likelihood (logged)
+## pfMLLik
+## particle filter unbiased estimate of marginal likelihood (logged)
 
 pfMLLik = function (n, simx0, t0, stepFun, dataLik, data) 
 {
@@ -11,14 +11,13 @@ pfMLLik = function (n, simx0, t0, stepFun, dataLik, data)
         for (i in 1:length(deltas)) {
             xmat[] = t(apply(xmat, 1, stepFun, t0 = times[i], deltat = deltas[i], 
                 ...))
-            w = apply(xmat, 1, dataLik, t = times[i + 1], y = data[i, 
-                ], log = FALSE, ...)
-            if (max(w) < 1e-20) {
-                warning("Particle filter bombed")
-                return(-1e+99)
-            }
-            ll = ll + log(mean(w))
-            rows = sample(1:n, n, replace = TRUE, prob = w)
+            lw = apply(xmat, 1, dataLik, t = times[i + 1], y = data[i, 
+                ], log = TRUE, ...)
+            m = max(lw)
+            rw = lw - m
+            sw = exp(rw)
+            ll = ll + m + log(mean(sw))
+            rows = sample(1:n, n, replace = TRUE, prob = sw)
             xmat[] = xmat[rows,]
         }
         ll
@@ -26,30 +25,5 @@ pfMLLik = function (n, simx0, t0, stepFun, dataLik, data)
 }
 
 
-pfMLLik.old <- function(n,simx0,t0,stepFun,dataLik,data)
-{
-  times=c(t0,as.numeric(rownames(data)))
-  deltas=diff(times)
-  return(function(...){
-    xmat=simx0(n,t0,...)
-    ll=0
-    for (i in 1:length(deltas)) {
-      xmat=t(apply(xmat,1,stepFun,t0=times[i],deltat=deltas[i],...))
-      w=apply(xmat,1,dataLik,t=times[i+1],y=data[i,],log=FALSE,...)
-      if (max(w)<1e-20) {
-        warning("Particle filter bombed")
-        return(-1e99)
-      }
-      ll=ll+log(mean(w))
-      rows=sample(1:n,n,replace=TRUE,prob=w)
-      xmat=xmat[rows,]
-    }
-    ll
-  })
-}
-
-
-
-
-# eof
+## eof
 
